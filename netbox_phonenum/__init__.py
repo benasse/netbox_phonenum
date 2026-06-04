29ec82ba131f49084ba19e17f830abe33d569490
Benoît Stahl <benasse@users.noreply.github.com>
2026-06-04 18:28:01 +0200

    remove netbox max_version

--- netbox_phonenum/__init__.py
+++ netbox_phonenum/__init__.py
@@ -12,7 +12,6 @@ class PhoneNumConfig(PluginConfig):
     author_email = 'jeremythorson@gmail.com'
     base_url = 'phonenum'
     min_version = "4.4.0"
-    max_version = "4.5.99"
     required_settings = []
     default_settings = {}
     caching_config = {
